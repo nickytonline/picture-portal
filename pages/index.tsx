01b8b12b194332f6546ddee5b9ac1ef6a294aae0
Nick Taylor <nick@iamdeveloper.com>
2021-10-16 13:04:21 -0400

    Metamask wallet connects to app now.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { keyframes } from '@emotion/react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 // Extend the window object.
 declare global {
@@ -27,25 +27,57 @@ const web3Styles = {
   },
 };
 
-const checkIfWalletIsConnected = () => {
-  /*
-   * First make sure we have access to window.ethereum
-   */
-  const { ethereum } = window;
-
-  if (!ethereum) {
-    console.log('Make sure you have metamask!');
-    return;
-  } else {
-    console.log('We have the ethereum object', ethereum);
-  }
-};
-
-function requestArt() {
-  console.log('implement me');
-}
-
 const Home: NextPage = () => {
+  const [currentAccount, setCurrentAccount] = useState('');
+
+  async function requestArt() {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log('Make sure you have metamask!');
+        return;
+      } else {
+        console.log('We have the ethereum object', ethereum);
+      }
+
+      /*
+       * Check if we're authorized to access the user's wallet
+       */
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log('Found an authorized account:', account);
+        setCurrentAccount(account);
+      } else {
+        console.log('No authorized account found');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   /*
    * This runs our function when the page loads.
    */
