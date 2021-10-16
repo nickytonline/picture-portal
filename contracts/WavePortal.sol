6fb5b48dbf1a3d93d499fc353a90f5ee0524f2f7
Nick Taylor <nick@iamdeveloper.com>
2021-10-16 00:03:33 -0400

    Now the contract has public functions.

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,22 @@ pragma solidity ^0.8.0;
 import 'hardhat/console.sol';
 
 contract WavePortal {
+    uint256 totalArtRequests;
+
     constructor() {
         console.log('This contract is dope and has some neurons!');
+    }
+
+    function askForArt() public {
+        totalArtRequests += 1;
+        console.log(unicode'%s requested some art! 🎨', msg.sender);
+    }
+
+    function getTotalArtRequests() public view returns (uint256) {
+        console.log(
+            unicode'We have %d total art requests! 🎨',
+            totalArtRequests
+        );
+        return totalArtRequests;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,19 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
-  console.log('We did it Joe! Contract deployed to:', waveContract.address);
+
+  console.log('Contract deployed to:', waveContract.address);
+  console.log('Contract deployed by:', owner.address);
+
+  let artRequestsCount;
+  artRequestsCount = await waveContract.getTotalArtRequests();
+
+  let waveTxn = await waveContract.askForArt();
+  await waveTxn.wait();
+
+  artRequestsCount = await waveContract.getTotalArtRequests();
 };
 
 const runMain = async () => {
