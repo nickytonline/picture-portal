3138d0ad2fdc115b04098435aa9b7bd91b0bb879
Nick Taylor <nick@iamdeveloper.com>
2021-10-15 23:31:27 -0400

    Added WavePortal contract and run script.

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import 'hardhat/console.sol';
+
+contract WavePortal {
+    constructor() {
+        console.log('This contract is dope and has some neurons!');
+    }
+}
---
+++ scripts/run.js
@@ -0,0 +1,18 @@
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+  console.log('We did it Joe! Contract deployed to:', waveContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
