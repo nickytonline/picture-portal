02178ac7180f7c322dc010f864bfc83a62b552d1
Nick Taylor <nick@iamdeveloper.com>
2021-10-16 01:21:48 -0400

    Updated hardhat config.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,21 @@
-import { HardhatUserConfig, task } from 'hardhat/config';
+import { HardhatUserConfig } from 'hardhat/config';
 import '@nomiclabs/hardhat-waffle';
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+require('dotenv').config();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const {
+  ALCHEMY_API_URL,
+  PRIVATE_RINKEBY_ACCOUNT_KEY = 'PRIVATE_RINKEBY_ACCOUNT_KEY',
+} = process.env;
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.4',
+  solidity: '0.8.0',
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_URL,
+      accounts: [PRIVATE_RINKEBY_ACCOUNT_KEY],
+    },
+  },
 };
 
 export default config;
