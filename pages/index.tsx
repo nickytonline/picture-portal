55206f874c8dd275629a4751deb19d38944b7154
Nick Taylor <nick@iamdeveloper.com>
2021-10-16 21:23:42 -0400

    Fixed HTTP Cat picture URLs.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,75 @@ import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 import abi from '../utils/WavePortal.json';
 
+const httpStatusCodes = [
+  '100',
+  '101',
+  '102',
+  '103',
+  '200',
+  '201',
+  '202',
+  '203',
+  '204',
+  '205',
+  '206',
+  '207',
+  '208',
+  '226',
+  '300',
+  '301',
+  '302',
+  '303',
+  '304',
+  '305',
+  '306',
+  '307',
+  '308',
+  '400',
+  '401',
+  '402',
+  '403',
+  '404',
+  '405',
+  '406',
+  '407',
+  '408',
+  '409',
+  '410',
+  '411',
+  '412',
+  '413',
+  '414',
+  '415',
+  '416',
+  '417',
+  '418',
+  '421',
+  '422',
+  '423',
+  '424',
+  '425',
+  '426',
+  '427',
+  '428',
+  '429',
+  '430',
+  '431',
+  '451',
+  '500',
+  '501',
+  '502',
+  '503',
+  '504',
+  '505',
+  '506',
+  '507',
+  '508',
+  '509',
+  '510',
+  '511',
+];
+
 declare global {
   namespace JSX {
     interface IntrinsicElements {
@@ -200,9 +269,9 @@ const Home: NextPage = () => {
       if (ethereum) {
         const wavePortalContract = getContract(ethereum);
 
-        const imageUrl = `https://http.cat/${
-          Math.floor(Math.random() * 599) + 99
-        }`;
+        const imageUrl = `https://http.cat/${Math.floor(
+          Math.random() * httpStatusCodes.length,
+        )}`;
 
         /*
          * Execute the actual wave from your smart contract
