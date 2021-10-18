1ea8a1c933dd5ccd54eb6972cf710a4fab885e41
Nick Taylor <nick@iamdeveloper.com>
2021-10-18 00:34:12 -0400

    Fixed useEffect lint issue.

--- pages/index.tsx
+++ pages/index.tsx
@@ -428,7 +428,7 @@ const Home: NextPage = () => {
     checkIfWalletIsConnected(ethereum);
     getArtRequests();
     document.querySelector('.Toastify')?.setAttribute('aria-live', 'polite');
-  }, []);
+  }, [getArtRequests]);
 
   return (
     <>
