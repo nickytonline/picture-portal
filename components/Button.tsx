583820cc63b5462bdc9df019d967fbd4655d4c14
Nick Taylor <nick@iamdeveloper.com>
2021-10-21 23:01:44 -0400

    Button props refactor.

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,18 +1,23 @@
-export const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> =
-  ({ children, onClick, type = 'button' }) => {
-    return (
-      <button
-        type={type}
-        sx={{
-          backgroundColor: 'accent',
-          color: '#fff',
-          borderRadius: '0.5rem',
-          border: 'none',
-          padding: '0.25rem 0.5rem',
-        }}
-        onClick={onClick}
-      >
-        {children}
-      </button>
-    );
-  };
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  type = 'button',
+}) => {
+  return (
+    <button
+      type={type}
+      sx={{
+        backgroundColor: 'accent',
+        color: '#fff',
+        borderRadius: '0.5rem',
+        border: 'none',
+        padding: '0.25rem 0.5rem',
+      }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
