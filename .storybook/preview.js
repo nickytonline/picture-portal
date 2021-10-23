214f11276de93a34345fd81f67ae2d33ff03b53e
Nick Taylor <nick@iamdeveloper.com>
2021-10-22 22:27:52 -0400

    Added a global Storybook decorator so Theme UI can be usde in the context of Storybook.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,15 @@
+import { ThemeProvider } from '@theme-ui/theme-provider';
+import { defaultTheme } from '../themes/defaultTheme';
+import React from 'react';
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={defaultTheme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
