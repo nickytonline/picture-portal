fa24eb3c01ef192a744a52ec8d1388eef7fc4e01
Nick Taylor <nick@iamdeveloper.com>
2021-10-22 20:41:02 -0400

    Added webpack aliases to Storybook webpack configuration.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,18 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../**/__stories__/*.stories.@(mdx|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        '@components': path.resolve(__dirname, '../components'),
+        '@assets': path.resolve(__dirname, '../assets'),
+      },
+    };
+
+    return config;
+  },
 };
