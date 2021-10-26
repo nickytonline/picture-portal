6e66b01f315508d5f2caf905b13f86aa4c128526
Dominic Duffin <dominicduffin@outlook.com>
2021-10-26 18:46:51 +0100

    Add imports for Next JS Images

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,17 @@ import { ThemeProvider } from '@theme-ui/theme-provider';
 import { defaultTheme } from '../themes/defaultTheme';
 import React from 'react';
 
+import '../styles/globals.css'; // so we have our global styles too
+import * as NextImage from 'next/image';
+
+// https://dev.to/jonasmerlin/how-to-use-the-next-js-image-component-in-storybook-1415
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={defaultTheme}>
